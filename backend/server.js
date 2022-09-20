c1cec267ac7287a570be1b8d6bdf492a2d9971e0
Mikołaj Wiśniewski <mikolajwisniewski01@gmail.com>
2022-09-21 00:16:58 +0200

    Another cors fix

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ const port = process.env.PORT || 5000
 
 connectDB()
 const app = express()
+app.use(cors())
 
 app.use('/news', require('./routes/newsRoutes'))
-app.use(cors())
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
